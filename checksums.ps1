4420fb4f089e7cf9da2d0ab3febc5a5b6285753c
foresterre <foresterre@ilumeo.com>
2016-09-04 19:18:54 +0200

    changed folder ordering

--- checksums.ps1
+++ checksums.ps1
@@ -10,55 +10,55 @@
 # purpose: Returns the sha256 hashes of all files, one directory deep,
 #          at the specified directory.
 #          For each file, a combination, 'hash file_name' is stored in a
-#          file called 'hashes.sha256'
-#
-# todo: compat. with recursive directories and files
-#       pointers (e.g.): Set-Location,
-#                        one output file per directory:
-#                           <root>/root1.bin
-#                           <root>/<subdir>/sub1.bin
-#
-#                        will become:
-#                           <root>/hashes.sha256 -> containing a hash for root1.bin
-#                           <root>/root1.bin
-#                           <root>/<subdir>/hashes.sha256 -> containing a hash for sub1.bin
-#                           <root>/<subdir>/sub1.bin
-#
+#          file called 'checksums.sha256'
+
 
 $dirArgument = $args[0]
+$requiredArgumentStr = "The first argument should be an existing directory. Usage: ./checksums [directory]"
 
 # verify input information
 if ($dirArgument -eq $null) {
     Write-Host "Error, no argument provided!"
+    Write-Host $requiredArgumentStr
     Exit
 }
 
 $dirTestPath = Test-Path $dirArgument
 if (-Not $dirTestPath) {
     Write-Host "Error, path not found!"
+    Write-Host $requiredArgumentStr
     Exit
 }
 
+
 # config
-$outputFileName = "hashes.sha256"
+$outputFileName = "checksums.sha256"
 $outputFilePath = Join-Path -Path $dirArgument -ChildPath $outputFileName
+
 
 # clean output
 $outputFileExist = Test-Path $outputFilePath
 if ($outputFileExist) {
+    Write-Host "Output file 'checksum.sha256' does already exist, cleaning file..."
     Clear-Content $outputFilePath
+} else {
+    Write-Host "Created output file: 'checksums.sha256\'".
+    Out-File $outputFileName
 }
 
-# process input
-$dirFiles = Get-ChildItem $dirArgument -File -Exclude *.sha256
 
-$dirFiles | Foreach-Object {
+# process input
+$dirFiles = Get-ChildItem $dirArgument -File | Where-Object { $_.Name -ne "checksums.sha256" }
+
+$dirFiles | ForEach-Object {
     $fPath = $_.FullName
     $hash = Get-FileHash $fPath -Algorithm SHA256
 
     $hashFmt = $hash.Hash
     $fNameFmt = $_.Name #Resolve-Path -Relative $fPath
     $resultFmt =  "$hashFmt $fNameFmt"
+
+    Write-Host "Appending hash: ", $resultFmt
 
     $resultFmt | Out-File $outputFilePath -Append
 }
